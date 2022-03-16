f3d9f044543171ca2d1b675a8e96b6641f67d4da
Khusniddin Madaminjonov <88139269+khusniddin26@users.noreply.github.com>
2022-03-16 17:33:24 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+function showCustomer(str) {
+  if (str == "") {
+    document.getElementById("txtHint").innerHTML = "";
+    return;
+  }
+  const xhttp = new XMLHttpRequest();
+  xhttp.onload = function() {
+    document.getElementById("txtHint").innerHTML = this.responseText;
+  }
+  xhttp.open("GET", "getcustomer.php?q="+str);
+  xhttp.send();
+}
